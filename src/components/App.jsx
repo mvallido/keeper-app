f642c75a2eb2fa7ab909ed422a3b6dad68ea8b75
m <vallido.michael@gmail.com>
2021-06-17 18:10:23 -0700

    Notes inside node.js are now seperate components, pulling from notes.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 function App(){
     return(
         <d1v>
             <Header />
+            {notes.map(noteCard => (
+                <Note 
+                    key={noteCard.key}
+                    title={noteCard.title}
+                    content={noteCard.content}
+                />
+            ))}
             <Footer />
-            <Note />
         </d1v>
     )
 }
