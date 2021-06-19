d322ea5337d5421c1a34a2aab245944740e5c7a1
m <vallido.michael@gmail.com>
2021-06-19 01:43:21 -0700

    Added note taking functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
+import { createNotEmittedStatement } from "typescript";
+
 
 function App(){
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNotes => {
+           return [...prevNotes, newNote];
+        })
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes => {
+           return prevNotes.filter((noteItem, index) => {
+                return index !== id;
+            })
+        })
+    }
+
     return(
         <d1v>
             <Header />
-            {notes.map(noteCard => (
-                <Note 
-                    key={noteCard.key}
-                    title={noteCard.title}
-                    content={noteCard.content}
+            <CreateArea onAdd={addNote} />
+            {notes.map((noteItem, index)=>{
+                return <Note 
+                    key={index}
+                    id={index}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    onDelete={deleteNote}
                 />
-            ))}
+            })}
             <Footer />
         </d1v>
     )
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 
 function Note(props){
+
+    function handleClick(){
+        props.onDelete(props.id);
+    }
+
     return(
         <d1v class="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
+            <button onClick={handleClick}>DELETE</button>
         </d1v>
     )
 }
